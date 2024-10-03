f44fbcdfc8751707c84d7099a7a1bc88b92b0373
Rehaboo <boo@rehaboo.com>
2024-10-03 18:27:24 +0300

    Update intro

--- Portfolio/src/components/Intro.vue
+++ Portfolio/src/components/Intro.vue
@@ -1,9 +1,9 @@
 <template>
     <v-sheet class="mx-auto" max-width="800px">
         <p class="px-8 py-16 text-h4 text-center text-uppercase">Tomi Vähämäki</p>
-        <p class="px-8 py-4 text-justify">I am a programmer, problem solver and generalist developer with a strong passion for games. I have been making and modding games for as long as I can remember and coding since the age of 11. I have taught myself to do it all from gameplay code, art and sound design to even writing my own game engines and renderers. I studied game development in Metropolia UAS and graduated in 2020.</p>
-        <p class="px-8 py-4 text-justify">Over the last few years my professional life has steered me towards AI and machine learning, where I have quickly become well versed in finding creative solutions to problems that may have been impossible to solve before.</p>
-        <p class="px-8 py-4 text-justify">I am currently working as CTO at EvokAI Creative Labs, where I am leading the development of a comprehensive AI-powered platform for brain health, using gamified solutions as data sources.</p>
+        <p class="px-8 py-4 text-justify">I am a programmer, problem solver and generalist developer with a strong passion for games. I started programming games when I was 11 years old and taught myself to do it all from gameplay code, art and sound design to even writing my own game engines and 3D renderers. I studied ICT and game development in Metropolia UAS and graduated in 2020.</p>
+        <p class="px-8 py-4 text-justify">Over the last few years my professional projects have steered me towards AI and machine learning, where I have quickly become well versed in finding creative solutions to problems that may have been impossible to solve before.</p>
+        <p class="px-8 py-4 text-justify">I am currently working as CTO and lead developer at EvokAI Creative Labs, where I am working on a comprehensive AI-powered platform for brain health, using gamified solutions as data sources.</p>
     </v-sheet>
 </template>
 
