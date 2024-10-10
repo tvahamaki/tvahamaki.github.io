3319e5770caa9314ee76dbbb5b2e47096d87078c
Rehaboo <boo@rehaboo.com>
2024-10-10 12:38:52 +0300

    Update intro

--- Portfolio/src/components/Intro.vue
+++ Portfolio/src/components/Intro.vue
@@ -1,9 +1,8 @@
 <template>
     <v-sheet class="mx-auto" max-width="800px">
         <p class="px-8 py-16 text-h4 text-center text-uppercase">Tomi Vähämäki</p>
-        <p class="px-8 py-4 text-justify">I am a programmer, problem solver and generalist developer with a strong passion for games. I started programming games when I was 11 years old and taught myself to do it all from gameplay code, art and sound design to even writing my own game engines and 3D renderers. I studied ICT and game development in Metropolia UAS and graduated in 2020.</p>
-        <p class="px-8 py-4 text-justify">Over the last few years my professional projects have steered me towards AI and machine learning, where I have quickly become well versed in finding creative solutions to problems that may have been impossible to solve before.</p>
-        <p class="px-8 py-4 text-justify">I am currently working as CTO and lead developer at EvokAI Creative Labs, where I am working on a comprehensive AI-powered platform for brain health, using gamified solutions as data sources.</p>
+        <p class="px-8 py-4 text-justify">I am a programmer, problem solver and generalist developer with a background in games, AI and medical technology. I have helped solve a wide variety of technical challenges while working on projects such as Rehaboo's AI-powered exercise game and EvokAI's data platform for brain health. I learned to code when I was 11 years old by making games, custom game engines and 3D renderers. I built a strong set of core skills, allowing me to learn new technologies quickly and tackle complex problems with confidence.</p>
+        <p class="px-8 py-4 text-justify">I am currently working as CTO and lead developer at EvokAI Creative Labs, where I am working on multiple medtech products with a focus on AI and data. These products include gamified solutions, simulations and sensors.</p>
     </v-sheet>
 </template>
 
